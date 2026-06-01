58216400378e9d8acb64848c0fd3df02c4b2170c
neelabhshukla <neelabhshukla79gmail.com>
2026-06-02 00:51:08 +0530

    Optimize blog publishing speed

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,19 +1,39 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({});
+const storage = new CloudinaryStorage({
+  cloudinary,
+
+  params: async (req, file) => ({
+    folder: "DevSpireUploads",
+    resource_type: "image",
+    transformation: [
+      {
+        width: 1200,
+        crop: "limit",
+        quality: "auto",
+        fetch_format: "auto",
+      },
+    ],
+  }),
+});
 
 const upload = multer({
   storage,
 
   limits: {
-    fileSize: 2 * 1024 * 1024, // 2MB
+    fileSize: 2 * 1024 * 1024,
   },
 
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
     } else {
-      cb(new Error("Only image files are allowed"), false);
+      cb(
+        new Error("Only image files are allowed"),
+        false
+      );
     }
   },
 });
