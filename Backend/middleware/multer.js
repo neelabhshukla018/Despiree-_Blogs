b8dc723378b3d107dc2b4d14be22f9d609e0bba3
neelabhshukla <neelabhshukla79gmail.com>
2026-06-02 16:47:17 +0530

    update in contactform

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -5,39 +5,49 @@ import cloudinary from "../config/cloudinary.js";
 const storage = new CloudinaryStorage({
   cloudinary,
 
-  params: async (req, file) => ({
-  folder: "DevSpireUploads",
-  resource_type: "auto",
-}),
+  params: async (req, file) => {
+
+    if (file.mimetype === "application/pdf") {
+      return {
+        folder: "DevSpireUploads",
+        resource_type: "raw",
+      };
+    }
+
+    return {
+      folder: "DevSpireUploads",
+      resource_type: "auto",
+    };
+  },
 });
 
 const upload = multer({
   storage,
 
   limits: {
-    fileSize: 2 * 1024 * 1024,
+    fileSize: 5 * 1024 * 1024,
   },
 
-fileFilter: (req, file, cb) => {
-  const allowedTypes = [
-    "image/",
-    "video/",
-    "audio/",
-    "application/pdf",
-    "application/msword",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  ];
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = [
+      "image/",
+      "video/",
+      "audio/",
+      "application/pdf",
+      "application/msword",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    ];
 
-  const allowed = allowedTypes.some(type =>
-    file.mimetype.startsWith(type)
-  );
+    const allowed = allowedTypes.some((type) =>
+      file.mimetype.startsWith(type)
+    );
 
-  if (allowed) {
-    cb(null, true);
-  } else {
-    cb(new Error("File type not supported"), false);
-  }
-},
+    if (allowed) {
+      cb(null, true);
+    } else {
+      cb(new Error("File type not supported"), false);
+    }
+  },
 });
 
 export default upload;
